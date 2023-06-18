b5a90b3ba980528dda5ac67b320cf2944d578a02
SobbleChao <89204645+SobbleChao@users.noreply.github.com>
2023-06-18 12:20:48 +0100

    Add files via upload

---
+++ Coding Project 3/javastuff.js
@@ -0,0 +1,21 @@
+function prev(){
+    document.getElementById('area_A1')
+    .src="images&gifs/bianco_hills1.webp"
+    innerHeight="200px"
+    innerWidth="300px";
+    document.getElementById('message_A1')
+    .innerHTML="Bianco Hills";
+    document.getElementById('message_B1')
+    .innerHTML="Bianco Hills is a hilly and picturesque countryside region in the center of Isle Delfino and northwest of Delfino Plaza";
+}
+
+function next(){
+    document.getElementById('area_A1')
+    .src="images&gifs/rico_harbour1.png"
+    innerHeight="200px"
+    innerWidth="300px";
+    document.getElementById('message_A1')
+    .innerHTML="Rico Harbour";
+    document.getElementById('message_B1')
+    .innerHTML="metal harbours foreign cousin"
+}
